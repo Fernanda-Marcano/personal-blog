e464614126e10ab0c6b037b8ffda65e2596dd27d
Fernanda-Marcano <marcanofernanda17@gmail.com>
2024-08-16 20:21:50 -0400

    Agregando los models de la base de datos

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Category, Article
 
 # Register your models here.
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    fields = ['name']
+
+admin.site.register(Category, CategoryAdmin)
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'pub_date')
+    fields = ['title', 'pub_date']
+
+admin.site.register(Article, ArticleAdmin)
--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(verbose_name='Categoría', max_length=50, blank=False, null=False, unique=True)
+    article_id = models.ForeignKey('Article', on_delete=models.RESTRICT, related_name='article')
+    
+    def __str__(self):
+        return self.name
+    
+    def clean(self):
+        if self.name:
+            self.name = self.name.strip().capitalize()
+    
+    class Meta:
+        db_table = 'Category'
+        ordering = ['name']
+        
+
+
+class Article(models.Model):
+    title = models.CharField(verbose_name='Título', max_length=100, blank=False, null=False, unique=True)
+    pub_date = models.DateField(default=date.today)
+    content = models.TextField(verbose_name='Contenido', blank=False, null=False)
+    
+    def __str__(self):
+        return self.title
+    
+    def clean(self):
+        if self.title:
+            self.title = self.title.strip().title()
+        if self.content:
+            self.content = self.content.strip()
+    
+    class Meta:
+        db_table = 'Article'
+        ordering = ['pub_date']
